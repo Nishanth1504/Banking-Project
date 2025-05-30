7678fef9857267d211f9a0ccef70dff4918ca801
Nishanth1504 <shrinivasnishanth0@gmail.com>
2025-05-30 12:33:56 +0530

    Update Axis Web.java

--- Axis Web.java
+++ Axis Web.java
@@ -1,12 +1,18 @@
+package com.example;
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class HelloServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, Web!</h1>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null) {
+            name = "World";
+        }
+        request.setAttribute("user", name);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
     }
 }
